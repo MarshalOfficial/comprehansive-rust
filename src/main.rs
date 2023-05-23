e63ea08a6f241345251eabcc4b1f0db1960dfdc6
MohammadReza <mr.rahmatzade371@gmail.com>
2023-05-24 01:57:50 +0330

    defining array

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-fn multiply(x: i16, y: i16) -> i16 {
-    x * y
-}
-
 fn main() {
-    let x: i8 = 15;
-    let y: i16 = 1000;
+    let array: [i32; 3] = [10,20,30];
 
-    println!("{x} * {y} = {}", multiply(x.into(), y));
+    println!("array: {array:?}");
 }
