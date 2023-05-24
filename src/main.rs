a1b2907b5b2b8ae232794c2cdcf1a7755dabe76e
MohammadReza <mr.rahmatzade371@gmail.com>
2023-05-24 13:06:31 +0330

    loop over array

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 fn main() {
     let array: [i32; 3] = [10,20,30];
+    print!("Iterating over array:");
+    for n  in array {
+        print!(" {n}");
+    }
+    println!();
 
-    println!("array: {array:?}");
+    print!("Iterating over array via foreach:");
+    array.into_iter().for_each(|n| {
+        print!(" {n}");
+    });
+    println!();
+
+    print!("Iterating over range:");
+    for i in 0..3{
+        print!(" {}", array[i]);
+    }
+    println!();
+
 }
