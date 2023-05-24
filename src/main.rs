1554e8103e6d21d602417980e34e2386a4042fe6
MohammadReza <mr.rahmatzade371@gmail.com>
2023-05-24 13:10:13 +0330

    transpose matrix

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,55 @@
+// ANCHOR: transpose
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    // ANCHOR_END: transpose
+    let mut result = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            result[j][i] = matrix[i][j];
+        }
+    }
+    return result;
+}
+
+// ANCHOR: pretty_print
+fn pretty_print(matrix: &[[i32; 3]; 3]) {
+    // ANCHOR_END: pretty_print
+    for row in matrix {
+        println!("{row:?}");
+    }
+}
+
+// ANCHOR: tests
+#[test]
+fn test_transpose() {
+    let matrix = [
+        [101, 102, 103], //
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301], //
+            [102, 202, 302],
+            [103, 203, 303],
+        ]
+    );
+}
+// ANCHOR_END: tests
+
+// ANCHOR: main
 fn main() {
-    let array: [i32; 3] = [10,20,30];
-    print!("Iterating over array:");
-    for n  in array {
-        print!(" {n}");
-    }
-    println!();
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
 
-    print!("Iterating over array via foreach:");
-    array.into_iter().for_each(|n| {
-        print!(" {n}");
-    });
-    println!();
+    println!("matrix:");
+    pretty_print(&matrix);
 
-    print!("Iterating over range:");
-    for i in 0..3{
-        print!(" {}", array[i]);
-    }
-    println!();
-
+    let transposed = transpose(matrix);
+    println!("transposed:");
+    pretty_print(&transposed);
 }
