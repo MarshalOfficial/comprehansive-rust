3b02dda475afbc415f800cab0ca7ee1f3093c724
MohammadReza <mr.rahmatzade371@gmail.com>
2023-05-25 23:49:15 +0330

    borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
-//While move semantics are the default, certain types are copied by default:
-//These types implement the Copy trait.
-fn main() {
-    let x = 42;
-    let y = x;
-    println!("x: {x}");
-    println!("y: {y}");
+#[derive(Debug)]
+struct Point(i32, i32);
 
-
-    //it depends on Point type
-    let p1 = Point(3, 4);
-    let p2 = p1;
-    println!("p1: {p1:?}");
-    println!("p2: {p2:?}");
-    
-    // After the assignment, both p1 and p2 own their own data.
-    // We can also use p1.clone() to explicitly copy the data.
-
+fn add(p1: &Point, p2: &Point) -> Point {
+    Point(p1.0 + p2.0, p1.1 + p2.1)
 }
 
-//You can opt-in your own types to use copy semantics:
-#[derive(Copy, Clone, Debug)]
-struct Point(i32, i32);
+fn main() {
+    let p1 = Point(3, 4);
+    let p2 = Point(10, 20);
+    let p3 = add(&p1, &p2);
+    println!("{p1:?} + {p2:?} = {p3:?}");
+}
