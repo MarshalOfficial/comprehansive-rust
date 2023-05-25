304205b3c379aec7df9ead83ca13d83526bf3543
MohammadReza <mr.rahmatzade371@gmail.com>
2023-05-25 22:26:58 +0330

    ownership

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
+struct Point(i32, i32);
+
 fn main() {
-    let a = 10;
-    println!("before: {a}");
-
     {
-        let a = "hello";
-        println!("inner scope: {a}");
-
-        let a = true;
-        println!("shadowed in inner scope: {a}");
+        let p = Point(3, 4);
+        println!("x: {}", p.0);
     }
-
-    println!("after: {a}");
-
-
-    let z = 1;
-    let g = &z;
-    let z = z + 1;
-    println!("{z} {g}");
+    println!("y: {}", p.1);
 }
