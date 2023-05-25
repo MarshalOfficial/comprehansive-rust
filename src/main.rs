5b85973019173a41355cd2427b4bf4e54c16fe6f
MohammadReza <mr.rahmatzade371@gmail.com>
2023-05-25 23:53:09 +0330

    constraints on borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-#[derive(Debug)]
-struct Point(i32, i32);
 
-fn add(p1: &Point, p2: &Point) -> Point {
-    Point(p1.0 + p2.0, p1.1 + p2.1)
-}
+
+// You can have one or more &T values at any given time, or
+// You can have exactly one &mut T value.
+
 
 fn main() {
-    let p1 = Point(3, 4);
-    let p2 = Point(10, 20);
-    let p3 = add(&p1, &p2);
-    println!("{p1:?} + {p2:?} = {p3:?}");
+    let mut a: i32 = 10;
+    let b: &i32 = &a;
+    
+    println!("b: {b}");
+
+    {
+        let c: &mut i32 = &mut a;
+        *c = 20;
+    }
+
+    println!("a: {a}");
+    //println!("b: {b}");
 }
